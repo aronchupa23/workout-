bfa55bc1f6a2ffaa9518debb2ef7e01cf42fc78f
aronchupa23 <naoki1194_boowy23@icloud.com>
2020-01-04 15:10:54 +0900

    [Finish]2-6

---
+++ Desktop/workout/ruby確認問題/figure.rb
@@ -0,0 +1,29 @@
+puts 100
+
+puts 100 + 100 
+
+puts 100 - 50
+
+puts 100 / 20 
+
+puts 20 * 500 
+
+puts "Rubyを学習"
+
+name = "鈴木さん"
+
+puts "hello" + name
+
+puts "こんにちわ#{name}"
+
+names = ["Perl", "Ruby", "Python", "Swift"]
+puts names[1]
+
+tall = {"太郎"=>185, "二郎"=>170, "花子"=>150}
+puts tall["太郎"]
+
+tall = {:太郎=>185, :二郎=>170, :花子=>150}
+puts tall[:花子]
+
+
+
